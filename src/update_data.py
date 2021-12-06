e6961ea4967bbc8e42bf23b091b00844fe0f3d28
rhrlima <ricardo_des@hotmail.com>
2021-12-05 22:27:13 -0300

    refactor of the update data

--- src/update_data.py
+++ src/update_data.py
@@ -1,22 +1,23 @@
-'''
-Downloads and updates the champions.json file when needed
-
-https://ddragon.leagueoflegends.com/api/versions.json
-http://ddragon.leagueoflegends.com/cdn/<version>/data/en_US/champion.json
-'''
-
 import os
 import json
 
 from urllib import request
 
-
-def _get_version_from_server(base_url):
-    with request.urlopen(base_url+'/api/versions.json') as f:
-        return json.loads(f.read().decode('utf-8'))[0]
+from sources import LOCAL_CHAMPION_DATA, VERSION_URL, CHAMPION_URL
 
 
-def _read_from_file(file_name):
+def _read_data_from_url(url: str):
+
+    with request.urlopen(url) as f:
+        return json.loads(f.read().decode('utf-8'))
+
+
+def _get_version_from_server(version_url: str):
+
+    return _read_data_from_url(version_url)[0]
+
+
+def _read_data_from_local(file_name: str):
 
     if not os.path.exists(file_name):
         return None
@@ -25,17 +26,19 @@ def _read_from_file(file_name):
         return json.loads(f.read())
 
 
-def _read_from_server(base_url, version):
-    with request.urlopen(base_url+f'/cdn/{version}/data/en_US/champion.json') as f:
-        return json.loads(f.read().decode('utf-8'))
+def _read_data_from_server(data_url: str):
+
+    return _read_data_from_url(data_url)
 
 
-def _save_data(file_name, data):
+def _save_data(file_name: str, data: dict):
+
     with open(file_name, 'w') as f:
         f.write(json.dumps(data))
 
 
-def _parse_data(data):
+def _parse_data(data: dict):
+
     parsed_data = {}
     tags_to_copy = ['id', 'key', 'name', 'title', 'info', 'tags']
 
@@ -44,7 +47,7 @@ def _parse_data(data):
     parsed_data['tags'] = {}
 
     for key in data['data'].keys():
-        
+
         if key not in parsed_data:
             parsed_data['data'][key] = {}
 
@@ -61,38 +64,37 @@ def _parse_data(data):
     return parsed_data
 
 
-def _compare_versions(ver1, ver2):
+def _compare_versions(ver1: str, ver2: str):
+    '''returns True is ver1 < ver2'''
 
-    ver1 = [int(v) for v in ver1.split('.')]# int(ver1.replace('.', ''))
-    ver2 = [int(v) for v in ver2.split('.')]
+    ver1 = list(map(int, ver1.split('.')))
+    ver2 = list(map(int, ver2.split('.')))
 
-    for v1, v2 in zip(ver1, ver2):
-        if v1 < v2:
-            return True
-    return False
+    return ver1 < ver2
 
 
-def get_updated_data(file_name='data/champions.json', force=False):
-    base_url = 'https://ddragon.leagueoflegends.com'
+def get_updated_data(file_name: str=LOCAL_CHAMPION_DATA, force: bool=False):
 
     print('reading local data')
-    data = _read_from_file(file_name)
-    s_ver = _get_version_from_server(base_url)
-    
-    if (data is None or _compare_versions(data['version'], s_ver)) or force:
+    data = _read_data_from_local(file_name)
+
+    # server version
+    s_ver = _get_version_from_server(VERSION_URL)
+
+    if data is None or force or _compare_versions(data['version'], s_ver):
 
         if data is None:
             print('no local data found')
 
-        elif _compare_versions(data['version'], s_ver):
+        elif force:
+            print('force update requested')
+
+        else:
             l_ver = data['version']
             print(f'local version {l_ver} older than server version {s_ver}')
 
-        if force:
-            print('force update requested')
-
         print('loading from server')
-        data = _read_from_server(base_url, s_ver)
+        data = _read_data_from_server(CHAMPION_URL.format(s_ver))
 
         print('parsing data')
         data = _parse_data(data)
@@ -100,9 +102,12 @@ def get_updated_data(file_name='data/champions.json', force=False):
         print('saving')
         _save_data(file_name, data)
 
-    print('data version is', data['version'])
+        print('data version updated to', data['version'])
+
+    else:
+        print('local data is up to date', data['version'])
 
 
 if __name__ == '__main__':
-    
-    get_updated_data(file_name='data/champions.json')
+
+    get_updated_data()
